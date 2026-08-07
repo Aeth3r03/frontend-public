7efa38b00eda47b180e4bef5774157ef849735f9
Aeth3r03 <waltereliperezrayo2@gmail.com>
2026-08-06 19:45:31 -0600

    setup inicial: Angular + Tailwind

---
+++ src/app/services/producto.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { environment } from "../../environments/environment";
+import { Producto } from "./producto";
+
+@Injectable({ providedIn: 'root' }) 
+export class ProductoService {
+    private apiUrl = `${environment.apiUrl}/productos`;
+
+    constructor(private http: HttpClient) {}
+
+    listar(nombre?: string): Observable<Producto[]> {
+        const url = nombre ? `${this.apiUrl}/?nombre=${nombre}` : `${this.apiUrl}/`;
+        return this.http.get<Producto[]>(url)
+    }
+}
