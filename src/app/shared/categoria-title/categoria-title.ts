9ef4820490c2b79c5f887aaeaaca522c09fb6226
Aeth3r03 <waltereliperezrayo2@gmail.com>
2026-09-01 16:26:03 -0600

    cambios en el ícono de la pagina e implementacion de lógica para obtener las imagenes de categorías

--- src/app/shared/categoria-title/categoria-title.ts
+++ src/app/shared/categoria-title/categoria-title.ts
@@ -3,6 +3,7 @@ import { Categoria } from '../../models/categoria';
 import { Producto } from '../../models/producto';
 import { CategoriaService } from '../../services/categoria.service';
 import { RouterLink } from '@angular/router';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-categoria-title',
@@ -16,13 +17,7 @@ export class CategoriaTitle {
   @Input() imageIndex: number = 0;
   productos = signal<Producto[]>([])
   private paleta = ['bg-primary/10', 'bg-accent/10', 'bg-purple-border/10', 'bg-line', 'bg-red-border/10'];
-  private imagenesPlaceholder = [
-    'https://images.unsplash.com/photo-1786057425168-1f326d4f47b1?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1573798484153-da43eda898f6?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1787585913070-a0c497a5ff28?q=80&w=689&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1784964279455-f0590dfb6694?q=80&w=736&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://plus.unsplash.com/premium_photo-1783100974590-1a86aa1e2ab7?q=80&w=764&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-  ];
+  private imagenPlaceholder = '/imagenes/categoria-placeholder.jpg';
 
   constructor(private categoriaService: CategoriaService) {};
 
@@ -41,7 +36,10 @@ export class CategoriaTitle {
   }
 
   get imageUrl(): string {
-    return this.imagenesPlaceholder[this.imageIndex % this.imagenesPlaceholder.length]
+    if (this.categoria.imagen_url) {
+      return environment.backendUrl + this.categoria.imagen_url;
+    }
+    return this.imagenPlaceholder;
   }
 
   get textoProductos(): string {
